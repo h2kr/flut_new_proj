1c73942d1c5a656077c0389409118158897368f6
h2kr <filapa@bk.ru>
2020-09-24 16:55:31 +0300

    message

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flut_new_proj/icons/app_icons.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(Game());
@@ -20,27 +21,144 @@ class Game extends StatelessWidget {
 
 class Home extends StatefulWidget {
   @override
- _StateHome createState() => _StateHome();
+  _StateHome createState() => _StateHome();
 }
 
 class _StateHome extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem> [
-            BottomNavigationBarItem(
-              icon: Icon(AppIcons.chart_bar),
-              title: Text("wdqdqs")
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(AppIcons.os),
-              title: Text("sdwdwd")
-            ),
-          ],
-        ),
+    int currentScreen = 2;
+
+    SvgPicture device = SvgPicture.asset(
+      "assets/icons/DevicePress.svg",
+      width: MediaQuery.of(context).size.width / 5,
     );
+    SvgPicture store = SvgPicture.asset(
+      "assets/icons/StorePress.svg",
+      width: MediaQuery.of(context).size.width / 5,
+    );
+    SvgPicture os = SvgPicture.asset(
+      "assets/icons/OsOnPress.svg",
+      width: MediaQuery.of(context).size.width / 5,
+    );
+    SvgPicture processes = SvgPicture.asset(
+      "assets/icons/ProcPress.svg",
+      width: MediaQuery.of(context).size.width / 5,
+    );
+    SvgPicture statistics = SvgPicture.asset(
+      "assets/icons/StatPress.svg",
+      width: MediaQuery.of(context).size.width / 5,
+    );
+
+    void changeColorButton(int index) {
+      setState(() {
+        switch (index) {
+          case 0:
+            device = SvgPicture.asset("assets/icons/DeviceOnPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+          case 1:
+            store = SvgPicture.asset("assets/icons/StoreOnPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 2:
+            os = SvgPicture.asset("assets/icons/OsOnPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 3:
+            processes = SvgPicture.asset("assets/icons/ProcOnPressed.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 4:
+            device = SvgPicture.asset("assets/icons/StatOnPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+        }
+
+        switch (currentScreen) {
+          case 0:
+            device = SvgPicture.asset("assets/icons/DevicePress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+          case 1:
+            store = SvgPicture.asset("assets/icons/StorePress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 2:
+            os = SvgPicture.asset("assets/icons/OsPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 3:
+            processes = SvgPicture.asset("assets/icons/ProcPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+
+          case 4:
+            device = SvgPicture.asset("assets/icons/StatPress.svg",
+                width: MediaQuery.of(context).size.width / 5);
+            break;
+        }
+
+        currentScreen = index;
+      });
+    }
+
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () {
+              changeColorButton(0);
+            },
+            child: device,
+          ),
+          GestureDetector(
+            onTap: () {
+              changeColorButton(1);
+            },
+            child: ButtonTheme(
+              minWidth: 0,
+              child: store,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              changeColorButton(2);
+            },
+            child: ButtonTheme(
+              minWidth: 0,
+              child: os,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              changeColorButton(3);
+            },
+            child: ButtonTheme(
+              minWidth: 0,
+              child: processes,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              changeColorButton(4);
+            },
+            child: ButtonTheme(
+              minWidth: 0,
+              child: statistics,
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
